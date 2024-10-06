1ee2f355158f349261e66dcc20f1ea69367f3a46
Jakkapat-Chongsuwat <70486906+Jakkapat-Chongsuwat@users.noreply.github.com>
2024-10-06 23:14:05 +0700

    [Add] omise configure

--- src/components/checkoutForm/omise-prebuilt-form/CheckoutCreditCard.js
+++ src/components/checkoutForm/omise-prebuilt-form/CheckoutCreditCard.js
@@ -1,14 +1,67 @@
 import React, { Component } from "react";
+import Script from "react-load-script";
 
 import "./Checkout.css";
 
+let OmiseCard;
+
 export class Checkout extends Component {
+  handleScriptLoad = () => {
+    OmiseCard = window.OmiseCard;
+    OmiseCard.configure({
+      publicKey: "pkey_test_61bn43chxcqmyo44sq5",
+      currency: "thb",
+      frameLabel: "Sabai shop",
+      submitLabel: "Pay Now",
+      buttonLabel: "Pay with Omise",
+    });
+  };
+
+  creditCardConfigure = () => {
+    OmiseCard.configure({
+      defaultPaymentMethod: "credit_card",
+      otherPaymentMethods: [],
+    });
+
+    OmiseCard.configureButton("#credit-card");
+
+    OmiseCard.attach();
+  };
+
+  omiseCardHandler = () => {
+    OmiseCard.open({
+      frameDescription: "Invoice #3847",
+      amount: 10000,
+      onCreateTokenSuccess: (token) => {
+        console.log(token);
+      },
+      onFormClosed: () => {},
+    });
+  };
+
+  handlePayWithCreditCardBtnclick = (e) => {
+    e.preventDefault();
+    this.creditCardConfigure();
+    this.omiseCardHandler();
+  };
+
   render() {
     return (
       <div className="own-form">
-        <button className="btn" type="button">
-          Pay with Credit Card
-        </button>
+        <Script
+          url="https://cdn.omise.co/omise.js"
+          onLoad={this.handleScriptLoad}
+        />
+        <form>
+          <button
+            id="credit-card"
+            className="btn"
+            type="button"
+            onClick={this.handlePayWithCreditCardBtnclick}
+          >
+            Pay with Credit Card
+          </button>
+        </form>
       </div>
     );
   }
